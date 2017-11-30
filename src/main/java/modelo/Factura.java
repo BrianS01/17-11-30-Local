8596cc2e4f6a4f65f3caf85f13dec30e99a38c53
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-29 23:30:06 -0500

    Local

--- src/main/java/modelo/Factura.java
+++ src/main/java/modelo/Factura.java
@@ -1,14 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
+
 package modelo;
 
-/**
- *
- * @author BRIAN
- */
-public class Factura {
+public class Factura
+{
+    private int idFactura;
+    private int idDetalle;
+    private int idProducto;
+    private int cantidad;
+    private int total;
+    private String metodoPago;
     
 }
